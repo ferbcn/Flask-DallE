575f9f5b366da74618be9525073721d2a3411bdf
garcifer <garcifer@students.zhaw.ch>
2023-01-24 19:29:26 +0100

    image browser

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fb432487413e7ef019fe2000b2fe94277565b7218be4e100'
 
-messages = [{'title': 'Message One',
-             'content': 'Hello World'},
-            {'title': 'Message Two',
-             'content': 'Hello Flask'}
-            ]
+images = [  {'title': 'Lasting Rainbow',
+         'content': 'Hello World',
+         'url': 'miro.jpg'},
+        {'title': 'Superb Firework',
+         'content': 'Hello Flask',
+         'url': 'rock.jpg'},
+        ]
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', messages=messages)
+    return render_template('index.html', images=images)
 
 
 @app.route('/create/', methods=('GET', 'POST'))
@@ -21,13 +23,14 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
+        url = request.form['url']
 
         if not title:
             flash('Title is required!')
         elif not content:
             flash('Content is required!')
         else:
-            messages.append({'title': title, 'content': content})
+            images.append({'title': title, 'content': content, 'url': url})
             return redirect(url_for('index'))
 
     return render_template('create.html')
@@ -35,4 +38,4 @@ def create():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
