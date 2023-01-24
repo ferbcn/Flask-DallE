d6f29e184fdf1097bb55b9d7e065f2dcf0533789
garcifer <garcifer@students.zhaw.ch>
2023-01-24 18:05:13 +0100

    simple form

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 # app.py
-from flask import Flask, request, jsonify
-app = Flask(__name__)
+from flask import Flask, render_template, request, url_for, flash, redirect
 
-# A welcome message to test our server
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fb432487413e7ef019fe2000b2fe94277565b7218be4e100'
+
+messages = [{'title': 'Message One',
+             'content': 'Hello World'},
+            {'title': 'Message Two',
+             'content': 'Hello Flask'}
+            ]
+
+
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return render_template('index.html', messages=messages)
+
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
