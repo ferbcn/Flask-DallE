e171510ddd652640b0a6d5fd2c1634e16ac113bd
garcifer <garcifer@students.zhaw.ch>
2023-02-02 11:00:55 +0100

    debug off

--- main.py
+++ main.py
@@ -141,4 +141,4 @@ def get_image_url(prompt):
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(host='0.0.0.0', threaded=True, port=80, debug=True)
+    app.run(host='0.0.0.0', threaded=True, port=80, debug=False)
