0a4306f5ad0adca3ae2ef0e41675f8ab66602f2f
garcifer <garcifer@students.zhaw.ch>
2023-02-04 16:47:00 +0100

    better scrolling with better dom manipulation

--- main.py
+++ main.py
@@ -302,28 +302,28 @@ def render_picture(data):
 @socketio.on('my_event')
 def handle_my_custom_event(json):
     last_img_id = json["data"]
-    next_images_count = 5
-    print(f"Loading {next_images_count} more images...")
+    print("last img id:", last_img_id)
+    print(f"Loading more images...")
+    new_image_count = 5
 
     # Hacky way for retrieving next images in db
     if last_img_id == 0:
-        images = FileContent.query.order_by(-FileContent.id).limit(10 + next_images_count).all()[10:]
+        images = FileContent.query.order_by(-FileContent.id).limit(10 + new_image_count).all()[10:]
     else:
-        n=0
         images = []
-        while n<5:
-            try:
-                last_img_id -= 1
-                next_image = FileContent.query.get(last_img_id)
+        found_img_count = 0
+        while (found_img_count < new_image_count):
+            # Iterate through DB: TODO: look-up sqlalchemy on this topic
+            last_img_id -= 1
+            next_image = FileContent.query.get(last_img_id)
+            if next_image is not None:
                 images.append(next_image)
-                n += 1
-            except Exception as e:
-                print(e)
+                found_img_count += 1
     # End of Hack
 
     images_json_data = []
     for image in images:
-        img = {"title": image.title, "rendered_data": image.rendered_data, "image_id": image.id}
+        img = {"title": image.title, "data": image.rendered_data, "image_id": image.id}
         images_json_data.append(img)
 
     print("Sending images to web-socket!")
--- static/index.js
+++ static/index.js
@@ -3,33 +3,49 @@ function handleClick() {
 }
 
 var last_img_id = 0;
+var waiting = false;
+
 
 // window scroll detect
 window.onscroll = function() {
 
-    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 100 && !waiting) {
+
         var socket = io();
         // websockets send more images
         socket.on('connect', function() {
             socket.emit('my_event', {data: last_img_id});
+            waiting = true;
+            var element = document.getElementById("spinner");
+            element.style.position = "absolut";
+            element.style.top = "500px";
+            element.style.visibility = 'visible';
         });
 
         socket.on('image feed', images => {
             //alert(images['title']);
-            images.forEach(add_image);
+            //images.forEach(add_image);
+            var all_nodes = [];
+            for (var i=0; i<images.length; i++){
+                //var node = "<div class='image_field'><center><h3>" + images[i]['title'] + "</h3></center><img src='data:image;base64," + images[i]['data'] + "'/></div>";
+                //all_nodes.push(node);
+                var new_image = document.createElement('div');
+                new_image.innerHTML += "<center><h3>" + images[i]['title'] + "</h3></center>";
+
+                new_image.classList.add('image_field');
+
+                var img = document.createElement('img');
+                img.src = "data:image;base64," + images[i]['data'];
+                new_image.appendChild(img);
+
+                document.getElementById('image_child').appendChild(new_image);
+            }
+            last_img_id = images[images.length-1]["image_id"];
+            //document.querySelector('#image_child').innerHTML += all_nodes;
+            document.getElementById('spinner').style.visibility = 'hidden';
+            waiting = false;
+
         });
-    }
 
-    // Add a new post with given contents to DOM.
-    // const post_template = Handlebars.compile(document.querySelector('#post').innerHTML);
-    function add_image(image) {
-        console.log(image['title'])
-        last_img_id = image["image_id"]
-        // Create new post.
-        // Handlebars
-        // const img = post_template({'title':data["user"], 'img_data': data["img_data"]});
-
-        // Add post to DOM.
-        document.querySelector('#image_child').innerHTML += "<div class='image_field'><h3>" + image['title'] + "</h3></center><img src='data:;base64," + image['rendered_data'] + "'/></div>"
     }
 }
